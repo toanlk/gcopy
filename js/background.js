45812036487f94a7abdf9262d1c0f92b38b4a659
Toan Le Khanh <toan.le@ntq-solution.com.vn>
2018-03-27 10:24:24 +0700

    change structure

--- js/background.js
+++ js/background.js
@@ -14,7 +14,36 @@ function executeScripts(tabId, injectDetailsArray) {
     callback(); // execute outermost function
 }
 
+function getGmailUrl() {
+  return "https://mail.google.com/mail/";
+}
+
+function isGmailUrl(url) {
+  // Return whether the URL starts with the Gmail prefix.
+  return url.startsWith(getGmailUrl());
+}
+
+function goToInbox() {
+  console.log('Going to inbox...');
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    for (var i = 0, tab; tab = tabs[i]; i++) {
+      if (tab.url && isGmailUrl(tab.url)) {
+        console.log('Found Gmail tab: ' + tab.url + '. ' +
+                    'Focusing and refreshing count...');
+        chrome.tabs.update(tab.id, {selected: true});
+        // startRequest({scheduleRequest:false, showLoadingAnimation:false});
+        return;
+      }
+    }
+    console.log('Could not find Gmail tab. Creating one...');
+    chrome.tabs.create({url: getGmailUrl()});
+  });
+}
+
 chrome.browserAction.onClicked.addListener(function (tab) {
+
+  goToInbox();
+
   executeScripts(null, [ 
       { file: "js/jquery-3.3.1.min.js" }, 
       { file: "js/moment-with-locales.min.js" },
---
+++ js/custom.js
@@ -0,0 +1,17 @@
+function searchReplace() {
+    if($("body > div.dw").find(".aoD").length > 0) {
+        var content = $("body > div.dw").find(".aoD").html();
+        content = $("input[name='subjectbox']").val();
+        content = content.replace("$date", today);
+        $("input[name='subjectbox']").val(content);
+        console.log(content);
+
+        $("body *").replaceText("$date", today);
+    }
+}
+
+(function() {
+    moment.locale('ja');
+    var today = moment().format('MMMM Do YYYY');
+    console.log(today);
+})();
